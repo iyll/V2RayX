2973620111e71ea830064c802baff2792c1e4910
iyl <iszhouyl@gmail.com>
2020-09-15 12:09:13 +0800

    Update dlcore.sh
    Support the latest version (v4.27.5) of v2ray-core.

--- V2RayX/dlcore.sh
+++ V2RayX/dlcore.sh
@@ -1,4 +1,4 @@
-VERSION="4.18.0"
+VERSION="4.27.5"
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BOLD='\033[1m'
@@ -14,21 +14,21 @@ if [ "$VERSION" != "$existingVersion" ]; then
     getCore=0
     mkdir -p v2ray-core-bin
     cd v2ray-core-bin
-    curl -s -L -o v2ray-macos.zip https://github.com/v2ray/v2ray-core/releases/download/v${VERSION}/v2ray-macos.zip
+    curl -s -L -o v2ray-macos-64.zip https://github.com/v2ray/v2ray-core/releases/download/v${VERSION}/v2ray-macos-64.zip
     if [[ $? == 0 ]]; then
-        unzip -o v2ray-macos.zip
+        unzip -o v2ray-macos-64.zip
         getCore=1
     else
-        unzip -o ~/Downloads/v2ray-macos.zip
+        unzip -o ~/Downloads/v2ray-macos-64.zip
         if [[ $? != 0 ]]; then
             getCore=0
         else
-            chmod +x v2ray-${VERSION}-macos/v2ray
-            output=$(v2ray-${VERSION}-macos/v2ray --version)
+            chmod +x v2ray-${VERSION}-macos-64/v2ray
+            output=$(v2ray-${VERSION}-macos-64/v2ray --version)
             existingVersion=${output:6:${#VERSION}}
             if [ "$VERSION" != "$existingVersion" ]; then
-                echo "${RED}v2ray-macos.zip in the Downloads folder does not contain version ${VERSION}."
-                echo "下载文件夹里的v2ray-macos.zip不是${VERSION}版本。${NORMAL}"
+                echo "${RED}v2ray-macos-64.zip in the Downloads folder does not contain version ${VERSION}."
+                echo "下载文件夹里的v2ray-macos-64.zip不是${VERSION}版本。${NORMAL}"
                 getCore=0
             else
                 getCore=1
@@ -37,8 +37,8 @@ if [ "$VERSION" != "$existingVersion" ]; then
     fi
     if [[ $getCore == 0 ]]; then
         echo "${RED}download failed!"
-        echo "Use whatever method you can think of, get v2ray-macos.zip of version ${VERSION} from v2ray.com, and put it in the folder 'Downloads' and try this script again."
-        echo "用你能想到任何办法，从 v2ray.com 下载好${VERSION}版本的 v2ray-macos.zip，放在“下载”文件夹里面，然后再次运行这个脚本。${NORMAL}"
+        echo "Use whatever method you can think of, get v2ray-macos-64.zip of version ${VERSION} from https://github.com/v2ray/v2ray-core/releases, and put it in the folder 'Downloads' and try this script again."
+        echo "用你能想到任何办法，从 https://github.com/v2ray/v2ray-core/releases 下载好${VERSION}版本的 v2ray-macos-64.zip，放在“下载”文件夹里面，然后再次运行这个脚本。${NORMAL}"
         exit 1
     fi
     chmod +x ./v2ray
